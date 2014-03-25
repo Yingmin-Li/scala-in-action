b2d702bf8c91acbf68684461aec63dcf07f0aaf6
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-03-25 18:53:36 +0100

    Automated resource management: Implementation

--- src/main/scala/AutomatedResourceManagement.scala
+++ src/main/scala/AutomatedResourceManagement.scala
@@ -3,20 +3,26 @@ import scala.io.Source
 
 object AutomatedResourceManagement {
 
+  type Closable = {
+    def close(): Unit
+  }
+
+  def withResource[A <: Closable, B](resource: A)(f: A => B): B =
+    try f(resource) finally resource.close()
+
   def main(args: Array[String]): Unit = {
     val in = new FileInputStream("build.sbt")
     val buildSbt =
-      try
-        Source.fromInputStream(in).getLines().mkString(f"%n")
-      finally
-        in.close() // Don't forget to close!
+      withResource(in) { resource =>
+        Source.fromInputStream(resource).getLines().mkString(f"%n")
+      }
     println(f"```%n$buildSbt%n```")
 
-    //    try {
-    //      in.read()
-    //      sys.error("IOExcetion was expected!")
-    //    } catch {
-    //      case _: java.io.IOException => () // That's expected!
-    //    }
+    try {
+      in.read() // This should throw an IOException for a closed resource!
+      sys.error("IOExcetion was expected!")
+    } catch {
+      case _: java.io.IOException => () // That's expected!
+    }
   }
 }
